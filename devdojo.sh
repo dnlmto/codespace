85f8aaf9179970e121f9cd4501c17deedc95121a
D.M. <59850465+dnlmto@users.noreply.github.com>
2023-11-10 01:49:17 +0000

    added slicing elements

--- devdojo.sh
+++ devdojo.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Ask the user for their name
-
-read -p "What is your name? " name
-
-# Greet the user
-echo "Hi there $name"
-echo "Welcome to DevDojo!"
+letters=( "A""B""C""D""E" ) 
+b=${letters:3}
+echo "${b}"
