2ece707c6e1792ddec122c6fef00f1c0f2264c2b
D.M. <59850465+dnlmto@users.noreply.github.com>
2023-11-10 01:22:26 +0000

    added user input commands

--- devdojo.sh
+++ devdojo.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-echo "Hello there" $1
+read -p "What is your name? " name
 
-# $1 : first parameter
-
-echo "Hello there" $2
-
-# $2 : second parameter
-
-echo "Hello there" $@
-
-# $@ : all
+echo "Hi there $name"
+echo "Welcome to DevDojo!"
